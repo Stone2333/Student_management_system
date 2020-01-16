ad2b9aaa6b86178938db69585b4bbae9bbbf358f
Stone233 <45417764+Stone2333@users.noreply.github.com>
2020-01-16 15:54:31 +0800

    sql补充

--- Student_management_system/mysql/mysql_query.py
+++ Student_management_system/mysql/mysql_query.py
@@ -206,13 +206,38 @@ def select_student_departments(departments):
 
 
 # 查询科目数量
-def select_course_number():
+def select_course_all_number():
     sql = '''
           SELECT COUNT(*) 
           FROM course
           '''
     number = select(sql)
     return number
+
+
+# 查询单个科目选择人数
+def selec_coures_people_number(course_id):
+    sql = '''
+          SELECT COUNT(*) AS number
+          FROM course
+          LEFT JOIN grade_table ON course.course_id=grade_table.course_id
+          WHERE course.course_id = "{}"
+          '''.format(course_id)
+    number = select(sql)
+    return number
+
+
+# 查询科目选择人数名单
+def select_coures_stundentinfo(course_id):
+    sql = '''
+             SELECT course.course_id,course.course_title,student_information.student_id,student_information.student_name,student_information.gender,student_information.birth,student_information.departments
+             FROM course
+             LEFT JOIN grade_table ON course.course_id=grade_table.course_id
+             LEFT JOIN student_information ON grade_table.student_id=student_information.student_id
+             WHERE course.course_id = "{}"
+             '''.format(course_id)
+    studentinfo = select(sql)
+    return  studentinfo
 
 
 # 查询所有科目信息
@@ -258,6 +283,67 @@ def select_course_id(course_id):
     return course_id
 
 
+
+# 单个学生总成绩
+def course_grade_sum(course_id):
+    sql = '''
+          SELECT student_information.student_id,student_name,SUM(grade)
+          FROM student_information
+          LEFT JOIN grade_table ON student_information.student_id=grade_table.student_id  
+          WHERE student_information.student_id = "{}"
+          '''.format(course_id)
+    course_grade_sum= select(sql)
+    return course_grade_sum
+
+
+
+# 所有学生总成绩  计算总成绩平均值有用
+def coures_grade_all_sum():
+    sql = '''
+          SELECT SUM(grade)
+          FROM grade_table
+          '''
+    course_grade_all_sum = select(sql)
+    return course_grade_all_sum
+
+
+
+# 单科平均值
+def course_grade_avg(course_id ):
+    sql = '''
+          SELECT AVG(grade) AS '平均成绩'
+          FROM grade_table
+          WHERE course_id = "{}"
+          '''.format(course_id )
+    course_grade_avg = select(sql)
+    return course_grade_avg
+
+
+# 单科最高
+def course_grade_max(course_id):
+    sql = '''
+          SELECT MAX(grade) AS '最高成绩'
+          FROM grade_table
+          WHERE course_id = "{}"
+          '''.format(course_id)
+    course_grade_max = select(sql)
+    return course_grade_max
+
+
+# 单科最低
+def course_grade_min(course_id):
+    sql = '''
+          SELECT MIN(grade) AS '最高成绩'
+          FROM grade_table
+          WHERE course_id = "{}"
+          '''.format(course_id)
+    course_grade_min = select(sql)
+    return course_grade_min
+
+
+
+
+
 # 查询所有成绩
 def select_all_grade():
     sql = '''
@@ -268,6 +354,7 @@ def select_all_grade():
           '''
     all_grade = select(sql)
     return all_grade
+
 
 
 # 根据学号查询学生成绩
